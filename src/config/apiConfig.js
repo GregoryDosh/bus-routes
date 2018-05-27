30530a1adebee254147bf27a7c48f92c18a62e67
GregoryDosh <GregoryDosh@users.noreply.github.com>
2018-05-27 10:26:04 -0500

    fixing capitalization

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,24 +1,25 @@
 import merge from 'lodash/merge'
 
 const baseConfig = {
-  stop: {
-    descriptionHost: (stopInt) => {
-      return `//localhost/stop=${stopInt}`
-    },
-    bussesHost: (stopInt) => {
-      return `//svc.metrotransit.org/NexTrip/${stopInt}?format=json`
+  departures: {
+    departuresAPI: (stopInt) => {
+      return ''
     },
   },
-  googleMapsAPIKey: 'AIzaSyC3XH68CcTNbGGsxnHBCowhW2OaV_ITF2g',
 }
 
 const environmentConfigs = {
   development: {
+    departures: {
+      departuresAPI: (stopInt) => {
+        return `https://localhost/graphql?&query={departures(stop_id:${stopInt}, map_width:800, map_height:600, map_scale:2){stop_id, full_map, update_time, stop_details{name}}}`
+      },
+    },
   },
   production: {
-    stop: {
-      descriptionHost: (stopInt) => {
-        return `https://www.metrotransit.org/nextrip/${stopInt}`
+    departures: {
+      departuresAPI: (stopInt) => {
+        return `https://nextrip.auengun.net/graphql?&query={departures(stop_id:${stopInt}, map_width:800, map_height:600, map_scale:2){stop_id, full_map, update_time, stop_details{name}}}`
       },
     },
   },
